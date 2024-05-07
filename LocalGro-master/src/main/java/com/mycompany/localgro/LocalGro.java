b4089e2888fb99edade4b235a896f75f4478b0df
Nalleswaran N <117887426+nalleswarann@users.noreply.github.com>
2024-05-07 15:45:30 +0530

    Add files via upload

---
+++ LocalGro-master/src/main/java/com/mycompany/localgro/LocalGro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.localgro;
+
+/**
+ *
+ * @author KURUGONDA VENU
+ */
+public class LocalGro {
+
+    public static void main(String[] args) {
+       
+    }
+}
